4097c3a68a8bb3b8aaf8ba6c50a3f98d48cd61e3
femsworld <femsworld@gmail.com>
2023-07-31 06:00:50 +0300

    Removed config file from tracking

--- backend/WebApi.Infrastructure/Database/DatabaseContext.cs
+++ backend/WebApi.Infrastructure/Database/DatabaseContext.cs
@@ -6,14 +6,20 @@ namespace WebApi.Infrastructure.Database
 {
     public class DatabaseContext : DbContext
     {
+        private readonly IConfiguration _configuration;
         public DbSet<Book> Books { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Address> Addresses { get; set; }     
 
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var builder = new NpgsqlDataSourceBuilder("Host=localhost; Server=localhost; Port=5432; Database=Library; Username=admin;Password=2003");
+            // var builder = new NpgsqlDataSourceBuilder("Host=localhost; Server=localhost; Port=5432; Database=Library; Username=admin;Password=2003");
+            var builder = new NpgsqlDataSourceBuilder(_configuration.GetConnectionString("DefaultConnection"));
             // var builder = new NpgsqlDataSourceBuilder(_configuration.GetConnectionString("DefaultConnection"));
             // builder.MapEnum<Role>();
             // optionsBuilder.AddInterceptors(new TimeStampInterceptor());
