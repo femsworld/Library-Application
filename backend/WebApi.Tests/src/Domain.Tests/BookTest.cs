61ef175aad9a114c33154c28e12b7a6c1fb0aec8
femsworld <femsworld@gmail.com>
2023-08-12 15:12:55 +0300

    Added Test project

---
+++ backend/WebApi.Tests/src/Domain.Tests/BookTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApi.Tests.src.Domain.Tests
+{
+    public class BookTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
