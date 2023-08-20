6efeacc0fc1a15e2573253eb875bab8c2f64ae4d
femsworld <femsworld@gmail.com>
2023-08-20 22:58:10 +0300

    Add JsonStringEnumConverter to SortOrder

--- backend/WebApi.Business/Services/Shared/SortOrder.cs
+++ backend/WebApi.Business/Services/Shared/SortOrder.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace WebApi.Business.Services.Shared
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum SortOrder
     {
         Ascending,
